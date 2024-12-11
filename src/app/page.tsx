8996a20f87fa6f33e7b17ce52215218894dade15
Adrian Popescu <adrianpopescu90@gmail.com>
2024-12-11 17:23:47 +0100

    Fix page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,18 +10,17 @@ export default function Home() {
     const [isMusicPlaying, setIsMusicPlaying] = useState(false)
 
     return (
-        <main
-            className="min-h-screen flex flex-col items-center justify-center bg-[#1a202c] overflow-hidden relative">
-            {isPlaying && <Snowfall/>}
+        <main className="h-screen flex flex-col items-center justify-between bg-[#1a202c] overflow-hidden relative">
+            {isPlaying && <Snowfall />}
             <div className="flex-grow flex items-center justify-center w-full">
                 {isPlaying ? (
-                    <HeroSection isMusicPlaying={isMusicPlaying} setIsMusicPlaying={setIsMusicPlaying}/>
+                    <HeroSection isMusicPlaying={isMusicPlaying} setIsMusicPlaying={setIsMusicPlaying} />
                 ) : (
-                    <PlayButton isPlaying={isPlaying} setIsPlaying={setIsPlaying}/>
+                    <PlayButton isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
                 )}
             </div>
             {isPlaying && (
-                <div className="absolute bottom-8 left-0 right-0 flex justify-center">
+                <div className="pb-8 w-full flex justify-center">
                     <PlayButton
                         isPlaying={isMusicPlaying}
                         setIsPlaying={setIsMusicPlaying}
